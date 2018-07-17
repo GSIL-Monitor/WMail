60f25ebfc44e089138f2f67287df27a688007734
gongw <gw_ling@sina.com>
2018-07-18 01:04:11 +0800

    add mailcore module, not finished

--- Qtec_SafeEmail_Android/mailcore/src/main/java/com/gongw/mailcore/imap/ImapService.java
+++ Qtec_SafeEmail_Android/mailcore/src/main/java/com/gongw/mailcore/imap/ImapService.java
@@ -1,21 +1,24 @@
 package com.gongw.mailcore.imap;
 
+
 import com.gongw.mailcore.setting.ConnectInfo;
-import com.libmailcore.ConnectionLogType;
 import com.libmailcore.ConnectionLogger;
-import com.libmailcore.IMAPConnectOperation;
+import com.libmailcore.IMAPAppendMessageOperation;
+import com.libmailcore.IMAPCopyMessagesOperation;
+import com.libmailcore.IMAPFetchContentOperation;
 import com.libmailcore.IMAPFetchFoldersOperation;
 import com.libmailcore.IMAPFetchMessagesOperation;
 import com.libmailcore.IMAPFolder;
-import com.libmailcore.IMAPFolderInfo;
 import com.libmailcore.IMAPFolderInfoOperation;
-import com.libmailcore.IMAPMessage;
+import com.libmailcore.IMAPOperation;
+import com.libmailcore.IMAPOperationProgressListener;
 import com.libmailcore.IMAPSession;
+import com.libmailcore.IMAPStoreFlagsRequestKind;
 import com.libmailcore.IndexSet;
 import com.libmailcore.Log;
 import com.libmailcore.MailException;
+import com.libmailcore.MessageFlag;
 import com.libmailcore.OperationCallback;
-
 import java.util.List;
 
 /**
@@ -23,13 +26,18 @@ import java.util.List;
  */
 
 public class ImapService {
-    private IMAPSession imapSession;
+    public IMAPSession imapSession;
     private ConnectionLogger connectionLogger;
-    private IMAPConnectOperation connectOperation;
+    private IMAPOperation connectOperation;
+    private IMAPOperation checkAccountOperation;
     private IMAPFetchFoldersOperation fetchFoldersOperation;
     private IMAPFolderInfoOperation folderInfoOperation;
     private IMAPFetchMessagesOperation fetchMessagesByNumberOperation;
-    private IMAPFetchMessagesOperation fetchMessagesByUidOperation;
+    private IMAPOperation storeFlagsByUidOperation;
+    private IMAPCopyMessagesOperation copyMessagesOperation;
+    private IMAPOperation expungeOperation;
+    private IMAPAppendMessageOperation appendMessageOperation;
+    private IMAPFetchContentOperation fetchAttachmentByUIDOperation;
 
     public ImapService(ConnectInfo connectInfo){
         imapSession = new IMAPSession();
@@ -63,7 +71,7 @@ public class ImapService {
             connectOperation.cancel();
         }
         disconnect();
-        connectOperation = (IMAPConnectOperation) imapSession.connectOperation();
+        connectOperation = imapSession.connectOperation();
         connectOperation.start(new OperationCallback() {
             @Override
             public void succeeded() {
@@ -75,6 +83,14 @@ public class ImapService {
 
             }
         });
+    }
+
+    public void checkAccount(OperationCallback callback){
+        if(checkAccountOperation != null){
+            checkAccountOperation.cancel();
+        }
+        checkAccountOperation = imapSession.checkAccountOperation();
+        checkAccountOperation.start(callback);
     }
 
     /**
@@ -109,60 +125,106 @@ public class ImapService {
      * 获取邮箱指定文件夹的邮件数、未读数等信息
      * @param path 文件夹路径
      */
-    public void folderInfo(String path){
+    public void folderInfo(String path, OperationCallback callback){
         if(folderInfoOperation !=null){
             folderInfoOperation.cancel();
         }
         folderInfoOperation = imapSession.folderInfoOperation(path);
-        folderInfoOperation.start(new OperationCallback() {
-            @Override
-            public void succeeded() {
-                IMAPFolderInfo folderInfo = folderInfoOperation.info();
-            }
-
-            @Override
-            public void failed(MailException e) {
-
-            }
-        });
+        folderInfoOperation.start(callback);
     }
 
-    public void fetchMessagesByNumber(String path, int kindFlag, IndexSet indexSet, List<String> extraHeaders){
+    /**
+     * 按位置序号获取邮件头
+     * @param path 文件夹路径
+     * @param messagesRequestKind 邮件头包含的数据种类
+     * @param indexSet 位置序号集合
+     * @param extraHeaders 额外的邮件头
+     * @param callback 回调接口
+     */
+    public void fetchMessagesByNumber(String path, int messagesRequestKind, IndexSet indexSet, List<String> extraHeaders, OperationCallback callback){
         if(fetchMessagesByNumberOperation!=null){
             fetchMessagesByNumberOperation.cancel();
         }
-        fetchMessagesByNumberOperation = imapSession.fetchMessagesByNumberOperation(path, kindFlag, indexSet);
+        fetchMessagesByNumberOperation = imapSession.fetchMessagesByNumberOperation(path, messagesRequestKind, indexSet);
         fetchMessagesByNumberOperation.setExtraHeaders(extraHeaders);
-        fetchMessagesByNumberOperation.start(new OperationCallback() {
-            @Override
-            public void succeeded() {
-                List<IMAPMessage> imapMessages = fetchMessagesByNumberOperation.messages();
-            }
-
-            @Override
-            public void failed(MailException e) {
-
-            }
-        });
+        fetchMessagesByNumberOperation.start(callback);
     }
 
-    public void fetchMessagesByUid(String path, int kindFlag, IndexSet indexSet, List<String> extraHeaders){
-        if(fetchMessagesByUidOperation!=null){
-            fetchMessagesByUidOperation.cancel();
+    public void flagMessage(String path, IndexSet uids, int flagsRequestKind, int messageFlag, OperationCallback callback){
+        if(storeFlagsByUidOperation != null){
+            storeFlagsByUidOperation.cancel();
         }
-        fetchMessagesByUidOperation = imapSession.fetchMessagesByUIDOperation(path, kindFlag, indexSet);
-        fetchMessagesByUidOperation.setExtraHeaders(extraHeaders);
-        fetchMessagesByUidOperation.start(new OperationCallback() {
+        storeFlagsByUidOperation = imapSession.storeFlagsByUIDOperation(path, uids, flagsRequestKind, messageFlag);
+        storeFlagsByUidOperation.start(callback);
+    }
+
+    public void copyMessages(String path, IndexSet uids, String destPath, OperationCallback callback){
+        if(copyMessagesOperation != null){
+            copyMessagesOperation.cancel();
+        }
+        copyMessagesOperation = imapSession.copyMessagesOperation(path, uids, destPath);
+        copyMessagesOperation.start(callback);
+    }
+
+    public void deleteMessages(final String path, IndexSet uids, final OperationCallback callback){
+        flagMessage(path, uids, IMAPStoreFlagsRequestKind.IMAPStoreFlagsRequestKindAdd, MessageFlag.MessageFlagDeleted, new OperationCallback() {
             @Override
             public void succeeded() {
-                List<IMAPMessage> imapMessages = fetchMessagesByUidOperation.messages();
+                if(expungeOperation != null){
+                    expungeOperation.cancel();
+                }
+                expungeOperation = imapSession.expungeOperation(path);
+                expungeOperation.start(callback);
             }
 
             @Override
             public void failed(MailException e) {
+                if(callback != null){
+                    callback.failed(e);
+                }
+            }
+        });
 
+    }
+
+    public void moveMessages(final String path, final IndexSet uids, String destPath, final OperationCallback callback){
+        copyMessages(path, uids, destPath, new OperationCallback(){
+
+            @Override
+            public void succeeded() {
+                deleteMessages(path, uids, callback);
+            }
+
+            @Override
+            public void failed(MailException e) {
+                if(callback != null){
+                    callback.failed(e);
+                }
             }
         });
     }
+
+    public void appendMessage(String path, byte[] bytes, int messageFlags, OperationCallback callback){
+        if(appendMessageOperation != null){
+            appendMessageOperation.cancel();
+        }
+        appendMessageOperation = imapSession.appendMessageOperation(path, bytes, messageFlags);
+        appendMessageOperation.start(callback);
+    }
+
+    public void fetchAttachmentByUIDOperation(String path, int uid, String partId, int encoding, OperationCallback callback){
+        if(fetchAttachmentByUIDOperation != null){
+            fetchAttachmentByUIDOperation.cancel();
+        }
+        fetchAttachmentByUIDOperation = imapSession.fetchMessageAttachmentByUIDOperation(path, uid, partId, encoding);
+        fetchAttachmentByUIDOperation.setProgressListener(new IMAPOperationProgressListener() {
+            @Override
+            public void bodyProgress(long current, long maximum) {
+
+            }
+        });
+        fetchAttachmentByUIDOperation.start(callback);
+    }
+
 
 }
---
+++ Qtec_SafeEmail_Android/mailcore/src/main/java/debug/MainActivity.java
@@ -0,0 +1,23 @@
+package debug;
+
+import com.gongw.common.base.BaseActivity;
+import com.gongw.login.R;
+import com.gongw.login.databinding.ActivityLoginBinding;
+
+/**
+ * Created by gongw on 2018/7/10.
+ */
+
+public class MainActivity extends BaseActivity<ActivityLoginBinding> {
+
+    @Override
+    public int getContentViewLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+}
